08b10c15823f1cb94d9faca56fea89abc0fe078c
Braxten Chenay <braxten509@hotmail.com>
2025-02-05 20:07:07 -0700

    Implemented all the simple methods for the ChessGame class

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -81,7 +81,7 @@ public class ChessGame {
 
     /**
      * Determines if the given team is in stalemate, which here is defined as having
-     * no valid move
+     * no valid mov
      *
      * @param teamColor which team to check for stalemate
      * @return True if the specified team is in stalemate, otherwise false
