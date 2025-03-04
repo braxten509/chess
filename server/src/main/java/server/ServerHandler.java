58efb35030cb27eae5bb64d44c5b79a52910fd5a
Braxten Chenay <braxten509@hotmail.com>
2025-03-03 20:29:11 -0700

    Refactored ServerHandler

--- server/src/main/java/server/ServerHandler.java
+++ server/src/main/java/server/ServerHandler.java
@@ -22,6 +22,45 @@ public class ServerHandler {
     authDataAccess
   );
 
+  /**
+   * turns a JSON string into an object
+   * @param req request object
+   * @param res response object
+   * @param classOfT type of object (Object.class)
+   * @return returns object 'T', whatever it is defined as
+   * @param <T> labels as a generic method
+   */
+  private static <T> T turnIntoObject(
+    Request req,
+    Response res,
+    Class<T> classOfT
+  ) {
+    return new Gson().fromJson(req.body(), classOfT);
+  }
+
+  /**
+   * @param string what the first parameter is
+   * @return "param1"
+   */
+  private static String turnIntoJson(String string) {
+    return new Gson().toJson(string);
+  }
+
+  /**
+   * @param keyword what the keyword is
+   * @param definition what the second parameter is
+   * @return {"param1", "definition of param1 [param2]"}
+   */
+  private static String turnIntoJson(String keyword, String definition) {
+    return new Gson().toJson(Map.of(keyword, definition));
+  }
+
+  /**
+   * Creates a new account for an unregistered user
+   * @param req request object
+   * @param res response object
+   * @return returns an Object, likely a JSON formatted String
+   */
   public static Object registerUser(Request req, Response res) {
     try {
       res.type("application/json");
@@ -35,20 +74,41 @@ public class ServerHandler {
         registerRequest.email() == null
       ) {
         res.status(400);
-        return new Gson().toJson(Map.of("message", "Error: bad request"));
+        return turnIntoJson("message", "Error: bad request");
       }
 
-      var result = userService.register(registerRequest);
-
+      var successResult = userService.register(registerRequest);
       res.status(200);
-      return new Gson().toJson(result);
+      return new Gson().toJson(successResult);
+
     } catch (DataAccessException e) {
       res.status(403);
-      return new Gson().toJson(Map.of("message", "Error: already taken"));
+      return turnIntoJson("message", "Error: already taken");
+
     } catch (Exception e) {
       res.status(500);
-      return new Gson().toJson(Map.of("message", "Error: " + e.getMessage()));
+      return turnIntoJson("message", "Error: " + e.getMessage());
     }
+  }
+
+  /**
+   * Logins an existing user
+   * @param req request object
+   * @param res response object
+   * @return returns an Object, likely a JSON formatted String
+   */
+  public static Object loginUser(Request req, Response res) {
+    res.type("application/json");
+
+    RegisterRequest registerRequest = turnIntoObject(req, res, RegisterRequest.class);
+
+    //    try {
+    //
+    //    } catch (DataAccessException e) {
+    //
+    //    }
+
+    return new Object();
   }
 
   public static Object clearDatabase(Request req, Response res) {
