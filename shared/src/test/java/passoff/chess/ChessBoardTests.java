8a1b81e485bd33b39197911d6bb43f35a2a59c4f
Braxten Chenay <braxten509@hotmail.com>
2025-03-31 22:20:36 -0600

    Fixed tests

--- shared/src/test/java/passoff/chess/ChessBoardTests.java
+++ shared/src/test/java/passoff/chess/ChessBoardTests.java
@@ -10,57 +10,50 @@ import org.junit.jupiter.api.Test;
 
 public class ChessBoardTests {
 
-  @Test
-  @DisplayName("Add and Get Piece")
-  public void getAddPiece() {
-    ChessPosition position = new ChessPosition(4, 4);
-    ChessPiece piece = new ChessPiece(
-      ChessGame.TeamColor.BLACK,
-      ChessPiece.PieceType.BISHOP
-    );
+    @Test
+    @DisplayName("Add and Get Piece")
+    public void getAddPiece() {
+        ChessPosition position = new ChessPosition(4, 4);
+        ChessPiece piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
 
-    var board = new ChessBoard();
-    board.addPiece(position, piece);
+        var board = new ChessBoard();
+        board.addPiece(position, piece);
 
-    ChessPiece foundPiece = board.getPiece(position);
+        ChessPiece foundPiece = board.getPiece(position);
 
-    Assertions.assertEquals(
-      piece.getPieceType(),
-      foundPiece.getPieceType(),
-      "ChessPiece returned by getPiece had the wrong piece type"
-    );
-    Assertions.assertEquals(
-      piece.getTeamColor(),
-      foundPiece.getTeamColor(),
-      "ChessPiece returned by getPiece had the wrong team color"
-    );
-  }
-
-  @Test
-  @DisplayName("Reset Board")
-  public void defaultGameBoard() {
-    var expectedBoard = TestUtilities.defaultBoard();
-
-    var actualBoard = new ChessBoard();
-    actualBoard.resetBoard();
-
-    Assertions.assertEquals(expectedBoard, actualBoard);
-  }
-
-  @Test
-  @DisplayName("Piece Move on All Pieces")
-  public void pieceMoveAllPieces() {
-    var board = new ChessBoard();
-    board.resetBoard();
-    for (int i = 1; i <= 8; i++) {
-      for (int j = 1; j <= 8; j++) {
-        ChessPosition position = new ChessPosition(i, j);
-        ChessPiece piece = board.getPiece(position);
-        if (piece != null) {
-          Assertions.assertDoesNotThrow(() -> piece.pieceMoves(board, position)
-          );
-        }
-      }
+        Assertions.assertEquals(piece.getPieceType(), foundPiece.getPieceType(),
+                "ChessPiece returned by getPiece had the wrong piece type");
+        Assertions.assertEquals(piece.getTeamColor(), foundPiece.getTeamColor(),
+                "ChessPiece returned by getPiece had the wrong team color");
     }
-  }
+
+
+    @Test
+    @DisplayName("Reset Board")
+    public void defaultGameBoard() {
+        var expectedBoard = TestUtilities.defaultBoard();
+
+        var actualBoard = new ChessBoard();
+        actualBoard.resetBoard();
+
+        Assertions.assertEquals(expectedBoard, actualBoard);
+    }
+
+
+    @Test
+    @DisplayName("Piece Move on All Pieces")
+    public void pieceMoveAllPieces() {
+        var board = new ChessBoard();
+        board.resetBoard();
+        for(int i = 1; i <= 8; i++) {
+            for(int j = 1; j <= 8; j++) {
+                ChessPosition position = new ChessPosition(i, j);
+                ChessPiece piece = board.getPiece(position);
+                if(piece != null) {
+                    Assertions.assertDoesNotThrow(() -> piece.pieceMoves(board, position));
+                }
+            }
+        }
+    }
+
 }
