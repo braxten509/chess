66287fcb9c28ac8872c8bee21b25c7e4ca86dd37
Braxten Chenay <braxten509@hotmail.com>
2025-04-02 23:58:06 -0600

    Fixed bugs in code

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import static server.ServerCommands.*;
+import static ui.EscapeSequences.SET_TEXT_COLOR_YELLOW;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import server.ServerCommands;
 import server.ServerFacade;
@@ -10,6 +12,48 @@ import ui.SpacingType;
  * Main class to control the client. Run this to open the client's perspective.
  */
 public class Main {
+
+  private static boolean validNumberOfParameters(
+      int expectedParameters,
+      String userInput
+  ) {
+
+    String[] userInputSpliced = userInput.split("\\s+");
+
+    int numberOfGivenParams = 0;
+    if (userInputSpliced.length > 1) {
+      numberOfGivenParams = Arrays.copyOfRange(userInputSpliced, 1, userInputSpliced.length).length;
+    }
+
+    return numberOfGivenParams == expectedParameters;
+  }
+
+  private static boolean expectedParameters(
+      int expectedNumberOfParams,
+      String regex,
+      String userInput
+  ) {
+    if (validNumberOfParameters(expectedNumberOfParams, userInput)) {
+
+      if (regex != null && userInput.matches(regex)) {
+        return true;
+      }
+      if (regex != null && !userInput.matches(regex)) {
+        printf("Improper type of parameters. Type 'help' for proper syntax.",
+            SpacingType.SURROUND, SET_TEXT_COLOR_YELLOW);
+        return false;
+      }
+      return regex == null;
+
+    } else {
+
+      printf("Improper number of parameters. Expected "
+              + expectedNumberOfParams + ". Type 'help' for proper syntax.",
+                SpacingType.SURROUND, SET_TEXT_COLOR_YELLOW);
+      return false;
+
+    }
+  }
 
   /**
    * Main method for the Main class.
@@ -33,86 +77,136 @@ public class Main {
 
     while (true) {
       System.out.print("[" + userStatus + "] >>> ");
-      String response = scanner.nextLine();
+      String userResponse = scanner.nextLine();
 
-      if (checkForQuit(response)) {
+      if (checkForQuit(userResponse)) {
         break;
       }
 
-      var loginPattern = "login\\s+[a-zA-Z0-9]+\\s+[^);]+";
-      var registerPattern = "register\\s+[a-zA-Z0-9]+\\s+[^);]+\\s+[a-zA-Z0-9@.&]+";
-      var createPattern = "create\\s+[a-zA-Z0-9]{1,20}";
-      var joinPattern = "join\\s+[0-9]+\\s+(white|black)";
-      var observePattern = "observe\\s+[0-9]+";
+      var loginRegex = "login\\s+[a-zA-Z0-9]+\\s+[^);]+";
+      var registerRegex = "register\\s+[a-zA-Z0-9]+\\s+[^);]+\\s+[a-zA-Z0-9@.&]+";
+      var createRegex = "create\\s+[a-zA-Z0-9]{1,20}";
+      var joinRegex = "join\\s+[0-9]+\\s+(white|black)";
+      var observeRegex = "observe\\s+[0-9]+";
 
       if (userStatus.equals("LOGGED_OUT")) {
-        switch (response.toLowerCase()) {
-          case "help" -> ServerCommands.helpCommand();
+        switch (userResponse.toLowerCase()) {
+          case String s when s.matches(
+              "\\bhelp\\b.*"
+          ) -> {
+            if (expectedParameters(0, null, userResponse)) {
+              ServerCommands.helpCommand();
+            }
+          }
 
           case String s when s.matches(
-              loginPattern
-          ) -> ServerCommands.loginCommand(serverFacade, response);
+              "\\blogin\\b.*"
+          ) -> {
+            if (expectedParameters(2, loginRegex, userResponse)) {
+              ServerCommands.loginCommand(serverFacade, userResponse);
+            }
+          }
 
           case String s when s.matches(
-              registerPattern
-          ) -> ServerCommands.registerCommand(serverFacade, response);
+              "\\bregister\\b.*"
+          ) -> {
+            if (expectedParameters(3, registerRegex, userResponse)) {
+              ServerCommands.registerCommand(serverFacade, userResponse);
+            }
+          }
 
-          case "clear" -> ServerCommands.clearCommand();
+          case String s when s.matches(
+              "\\bclear\\b.*"
+          ) -> {
+            if (expectedParameters(0, null, userResponse)) {
+              ServerCommands.clearCommand();
+            }
+          }
 
-          case String s when s.matches(".*register.*") -> printf(
-              "Please input entire command.",
-              SpacingType.SURROUND,
-              EscapeSequences.SET_TEXT_COLOR_YELLOW
-          );
-
-          case String s when s.matches(".*login.*") -> printf(
-              "Please input entire command.",
-              SpacingType.SURROUND,
-              EscapeSequences.SET_TEXT_COLOR_YELLOW
-          );
-
-          default -> printf(
-              "Unknown or unavailable command. Type 'help' for a list of valid commands.",
-              SpacingType.UNDER,
-              EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY
-          );
-
+          default -> {
+            if (
+                !userResponse.matches("\\bquit\\b\\s*\\S+")
+                    && !userResponse.matches("\\bexit\\b\\s*\\S+")
+            ) {
+              printf(
+                  "Unknown or unavailable command. Type 'help' for a list of valid commands.",
+                  SpacingType.UNDER,
+                  EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY
+              );
+            }
+          }
         }
       } else {
-        switch (response.toLowerCase()) {
-          case "help" -> ServerCommands.helpCommand();
-
-          case "logout" -> ServerCommands.logoutCommand(serverFacade);
+        switch (userResponse.toLowerCase()) {
+          case String s when s.matches(
+              "\\bhelp\\b.*"
+          ) -> {
+            if (expectedParameters(0, null, userResponse)) {
+              ServerCommands.helpCommand();
+            }
+          }
 
           case String s when s.matches(
-                createPattern
-          ) -> ServerCommands.createCommand(serverFacade, response);
+              "\\blogout\\b.*"
+          ) -> {
+            if (expectedParameters(0, null, userResponse)) {
+              ServerCommands.logoutCommand(serverFacade);
+            }
+          }
 
-          case "list" -> ServerCommands.listCommand(serverFacade);
+          case String s when s.matches(
+              "\\bcreate\\b.*"
+          ) -> {
+            if (expectedParameters(1, createRegex, userResponse)) {
+              ServerCommands.createCommand(serverFacade, userResponse);
+            }
+          }
 
-          case String s2 when s2.matches(
-                joinPattern
-          ) -> ServerCommands.joinCommand(serverFacade, response);
+          case String s when s.matches(
+              "\\blist\\b.*"
+          ) -> {
+            if (expectedParameters(0, null, userResponse)) {
+              ServerCommands.listCommand(serverFacade);
+            }
+          }
 
-          case String s3 when s3.matches(
-                observePattern
-          ) -> ServerCommands.observeCommand(
-              Integer.parseInt(response.split("\\s")[1])
-          );
+          case String s when s.matches(
+              "\\bjoin\\b.*"
+          ) -> {
+            if (expectedParameters(2, joinRegex, userResponse)) {
+              ServerCommands.joinCommand(serverFacade, userResponse);
+            }
+          }
 
-          case "clear" -> ServerCommands.clearCommand();
+          case String s when s.matches(
+              "\\bobserve\\b.*"
+          ) -> {
+            if (expectedParameters(1, observeRegex, userResponse)) {
+              int gameId = Integer.parseInt(userResponse.split("\\s")[1]);
+              ServerCommands.observeCommand(gameId);
+            }
+          }
 
-          case "create", "join", "observe" -> printf(
-              "Please input entire command.",
-              SpacingType.SURROUND,
-              EscapeSequences.SET_TEXT_COLOR_YELLOW
-          );
+          case String s when s.matches(
+              "\\bclear\\b.*"
+          ) -> {
+            if (expectedParameters(0, null, userResponse)) {
+              ServerCommands.clearCommand();
+            }
+          }
 
-          default -> printf(
-              "Unknown or unavailable command. Type 'help' for a list of valid commands.",
-              SpacingType.UNDER,
-              EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY
-          );
+          default -> {
+            if (
+                !userResponse.matches("\\bquit\\b\\s*\\S+")
+                && !userResponse.matches("\\bexit\\b\\s*\\S+")
+            ) {
+              printf(
+                  "Unknown or unavailable command. Type 'help' for a list of valid commands.",
+                  SpacingType.UNDER,
+                  EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY
+              );
+            }
+          }
         }
       }
     }
--- client/src/main/java/server/ServerCommands.java
+++ client/src/main/java/server/ServerCommands.java
@@ -21,254 +21,10 @@ public class ServerCommands {
 
   private static final HashMap<Integer, Integer> LISTED_GAMES = new HashMap<>();
 
-  /**
-   * Returns the same output as System.out.print() but with formatting
-   *
-   * @param text       text to print (everything will be formatted as specified)
-   * @param formatting formatting. Include multiple with '+'
-   * @param spacing    specifies spacing around text
-   */
-  public static void printf(
-      String text,
-      SpacingType spacing,
-      String formatting
-  ) {
-    String spacingBeginning = "";
-    String spacingEnding = "\n";
-
-    switch (spacing) {
-      case NONE -> spacingEnding = "";
-      case ABOVE -> spacingBeginning += "\n";
-      case UNDER -> spacingEnding += "\n";
-      case SURROUND -> {
-        spacingBeginning += "\n";
-        spacingEnding += "\n";
-      }
-      case DOUBLE_SURROUND -> {
-        spacingBeginning += "\n\n";
-        spacingEnding += "\n\n";
-      }
-      case null, default -> {
-      }
-    }
-
-    if (formatting != null) {
-      System.out.print(formatting);
-    }
-
-    System.out.print(spacingBeginning + text + spacingEnding);
-
-    System.out.print(RESET_ALL);
-  }
-
-  /**
-   * Command used to clear the client's terminal.
-   */
-  public static void clearCommand() {
-    printf("", SpacingType.REGULAR, ERASE_SCREEN);
-    System.out.flush();
-    printf(
-        "♕ Welcome to Chess! Type 'help' to get started ♕",
-        SpacingType.UNDER,
-        null
-    );
-  }
-
-  /**
-   * Helper method to check for when the user tries to exit a text input area.
-   *
-   * @param response the response the user gives to the input request
-   * @return true or false based on if decides to quit or not (checks for "quit" and "exit")
-   */
-  public static boolean checkForQuit(String response) {
-    return (
-        response.equalsIgnoreCase("quit") | response.equalsIgnoreCase("exit")
-      );
-  }
-
-  /**
-   * Lists available commands for the user.
-   */
-  public static void helpCommand() {
-    printf(
-        "AVAILABLE COMMANDS",
-        SpacingType.SURROUND,
-        SET_TEXT_COLOR_YELLOW + SET_TEXT_BOLD
-    );
-    printf("help", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
-    printf(" : lists all available commands", SpacingType.REGULAR, null);
-    if (userStatus.equals("LOGGED_OUT")) {
-      printf(
-          "login <username> <password>",
-          SpacingType.NONE,
-          SET_TEXT_COLOR_YELLOW
-      );
-      printf(" : login an existing user", SpacingType.REGULAR, null);
-      printf(
-          "register <username> <password> <email>",
-          SpacingType.NONE,
-          SET_TEXT_COLOR_YELLOW
-      );
-      printf(" : register a new user", SpacingType.REGULAR, null);
-    } else {
-      printf("logout", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
-      printf(" : logout of current session", SpacingType.REGULAR, null);
-      printf("create <name>", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
-      printf(" : create a new game", SpacingType.REGULAR, null);
-      printf("list", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
-      printf(" : list games", SpacingType.REGULAR, null);
-      printf(
-          "join <ID> [WHITE|BLACK]",
-          SpacingType.NONE,
-          SET_TEXT_COLOR_YELLOW
-      );
-      printf(" : join an existing game", SpacingType.REGULAR, null);
-      printf("observe <ID>", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
-      printf(" : watch an ongoing game", SpacingType.REGULAR, null);
-    }
-    printf("clear", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
-    printf(" : clears the current terminal", SpacingType.REGULAR, null);
-    printf("quit", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
-    printf(" or ", SpacingType.NONE, null);
-    printf("exit", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
-    printf(" : exit the program", SpacingType.UNDER, null);
-  }
-
-  /**
-   * Command to log in the user.
-   *
-   * @param serverFacade serverFacade to use
-   * @param input user input to use
-   */
-  public static void loginCommand(ServerFacade serverFacade, String input) {
-    String username = input.split("\\s")[1];
-    String password = input.split("\\s")[2];
-
-    try {
-      AuthData authData = serverFacade.loginUser(username, password);
-      authToken = authData.authToken();
-      loadValues(serverFacade);
-
-      printf(
-          "Success! You have been logged in",
-          SpacingType.SURROUND,
-          SET_TEXT_COLOR_GREEN
-      );
-      userStatus = username.toUpperCase();
-    } catch (Exception e) {
-      printf(
-          "Error: Invalid credentials",
-          SpacingType.SURROUND,
-          SET_TEXT_COLOR_RED
-      );
-    }
-  }
-
-  /**
-   * Command to log out the user.
-   *
-   * @param serverFacade serverFacade to use
-   */
-  public static void logoutCommand(ServerFacade serverFacade) {
-    try {
-      serverFacade.logoutUser(authToken);
-      authToken = "";
-      userStatus = "LOGGED_OUT";
-      printf("Logout successful!", SpacingType.SURROUND, SET_TEXT_COLOR_GREEN);
-    } catch (Exception e) {
-      printf(
-          "Error logging out: " + e.getMessage(),
-          SpacingType.SURROUND,
-          SET_TEXT_COLOR_RED
-      );
-    }
-  }
-
-  /**
-   * Command used to register a new user.
-   *
-   * @param serverFacade serverFacade to use
-   * @param input user input to use
-   */
-  public static void registerCommand(ServerFacade serverFacade, String input) {
-    String username = input.split("\\s")[1];
-    String password = input.split("\\s")[2];
-    String email = input.split("\\s")[3];
-
-    printf("\nPlease confirm Password: ", SpacingType.NONE, SET_TEXT_BOLD);
-    String confirmedPassword = SCANNER.next();
-
-    if (!password.equals(confirmedPassword)) {
-      printf(
-          "Error: Passwords do not match.",
-          SpacingType.SURROUND,
-          SET_TEXT_COLOR_RED
-      );
-      return;
-    }
-
-    try {
-      serverFacade.registerUser(username, password, email);
-
-      AuthData authData = serverFacade.loginUser(username, password);
-      authToken = authData.authToken();
-
-      printf(
-          "Success! You have been registered and logged in",
-          SpacingType.SURROUND,
-          SET_TEXT_COLOR_GREEN
-      );
-      userStatus = username.toUpperCase();
-    } catch (Exception e) {
-      if (
-          e
-              .getMessage()
-              .equals(
-                  "java.lang.RuntimeException: (403) ERROR: {\"message\":\"Error: already taken\"}"
-              )
-      ) {
-        printf(
-            "Error: Username already taken",
-            SpacingType.REGULAR,
-            SET_TEXT_COLOR_RED
-        );
-      }
-    }
-  }
-
-  /**
-   * Command used to create a new game.
-   *
-   * @param serverFacade serverFacade to use
-   * @param input user input to use
-   */
-  public static void createCommand(ServerFacade serverFacade, String input) {
-    String gameName = input.split("\\s+")[1];
-    try {
-      serverFacade.createGame(authToken, gameName);
-      loadValues(serverFacade);
-      printf(
-          "Success creating game with name '" + gameName + "'!",
-          SpacingType.SURROUND,
-          SET_TEXT_COLOR_GREEN
-      );
-    } catch (Exception e) {
-      printf(
-          "Error creating game: " + e.getMessage(),
-          SpacingType.SURROUND,
-          SET_TEXT_COLOR_RED
-      );
-    }
-  }
-
-  /**
-   * Help command that displays the screen the player sees once in a game.
-   *
-   * @param playerColor color of the player that joined
-   */
   private static void inGame(String playerColor) {
     String[][] chessPieceGrid = getChessPieceGrid(playerColor);
 
+    /* BlackSquare = 1, WhiteSquare = 0 */
     int squareColor = (playerColor.equals("WHITE")) ? 1 : 0;
     for (int indexX = 8; indexX > 0; indexX--) {
       for (int indexY = 0; indexY < 8; indexY++) {
@@ -313,8 +69,8 @@ public class ServerCommands {
 
     // game loop
     while (true) {
-      System.out.print("[" + userStatus + " - " + playerColor + "] >>> ");
-      String response = SCANNER.next();
+      System.out.print("[" + userStatus + " - " + playerColor.toUpperCase() + "] >>> ");
+      String response = SCANNER.nextLine();
 
       if (checkForQuit(response)) {
         break;
@@ -326,7 +82,7 @@ public class ServerCommands {
 
   private static String[][] getChessPieceGrid(String playerColor) {
     String[][] chessPieceGrid = {{}};
-    if (playerColor.equalsIgnoreCase("white")) {
+    if (playerColor.equalsIgnoreCase("white") || playerColor.equalsIgnoreCase("observer")) {
       // starts at bottom 0,0
       chessPieceGrid = new String[][]{
           {
@@ -378,15 +134,283 @@ public class ServerCommands {
     return chessPieceGrid;
   }
 
+  private static void loadValues(ServerFacade serverFacade) {
+    ArrayList<GameData> games = serverFacade.listGames(authToken).games();
+    LISTED_GAMES.clear();
+    int gameNumber = 1;
+
+    for (GameData game : games) {
+      LISTED_GAMES.put(gameNumber, game.gameID());
+      gameNumber += 1;
+    }
+  }
+
+  /**
+   * Returns the same output as System.out.print() but with formatting
+   *
+   * @param text       text to print (everything will be formatted as specified)
+   * @param formatting formatting. Include multiple with '+'
+   * @param spacing    specifies spacing around text
+   */
+  public static void printf(
+      String text,
+      SpacingType spacing,
+      String formatting
+  ) {
+    String spacingBeginning = "";
+    String spacingEnding = "\n";
+
+    switch (spacing) {
+      case NONE -> spacingEnding = "";
+      case ABOVE -> spacingBeginning += "\n";
+      case UNDER -> spacingEnding += "\n";
+      case SURROUND -> {
+        spacingBeginning += "\n";
+        spacingEnding += "\n";
+      }
+      case DOUBLE_SURROUND -> {
+        spacingBeginning += "\n\n";
+        spacingEnding += "\n\n";
+      }
+      case null, default -> {
+      }
+    }
+
+    if (formatting != null) {
+      System.out.print(formatting);
+    }
+
+    System.out.print(spacingBeginning + text + spacingEnding);
+
+    System.out.print(RESET_ALL);
+  }
+
+  /**
+   * Command used to clear the client's terminal.
+   */
+  public static void clearCommand() {
+
+    printf("", SpacingType.REGULAR, ERASE_SCREEN);
+    System.out.flush();
+    printf(
+        "♕ Welcome to Chess! Type 'help' to get started ♕",
+        SpacingType.UNDER,
+        null
+    );
+  }
+
+  /**
+   * Helper method to check for when the user tries to exit a text userInput area.
+   *
+   * @param userInput the response the user gives to the userInput request
+   * @return true or false based on if decides to quit or not (checks for "quit" and "exit")
+   */
+  public static boolean checkForQuit(String userInput) {
+    if (userInput.matches("\\bquit\\b\\s*\\S+") || userInput.matches("\\bexit\\b\\s*\\S+")) {
+      printf("Improper number of parameters. Expected 0. Use: <quit/exit>",
+          SpacingType.SURROUND, SET_TEXT_COLOR_YELLOW);
+      return false;
+    } else {
+      return userInput.matches("\\bquit\\b") || userInput.matches("\\bexit\\b");
+    }
+  }
+
+  /**
+   * Lists available commands for the user.
+   */
+  public static void helpCommand() {
+
+    printf(
+        "AVAILABLE COMMANDS",
+        SpacingType.SURROUND,
+        SET_TEXT_COLOR_YELLOW + SET_TEXT_BOLD
+    );
+    
+    printf("help", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
+    printf(" : lists all available commands", SpacingType.REGULAR, null);
+    
+    if (userStatus.equals("LOGGED_OUT")) {
+      printf(
+          "login <username> <password>",
+          SpacingType.NONE,
+          SET_TEXT_COLOR_YELLOW
+      );
+      
+      printf(" : login an existing user", SpacingType.REGULAR, null);
+      printf(
+          "register <username> <password> <email>",
+          SpacingType.NONE,
+          SET_TEXT_COLOR_YELLOW
+      );
+      
+      printf(" : register a new user", SpacingType.REGULAR, null);
+      
+    } else {
+      
+      printf("logout", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
+      printf(" : logout of current session", SpacingType.REGULAR, null);
+      printf("create <name>", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
+      printf(" : create a new game", SpacingType.REGULAR, null);
+      printf("list", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
+      printf(" : list games", SpacingType.REGULAR, null);
+      
+      printf(
+          "join <ID> [WHITE|BLACK]",
+          SpacingType.NONE,
+          SET_TEXT_COLOR_YELLOW
+      );
+      
+      printf(" : join an existing game", SpacingType.REGULAR, null);
+      printf("observe <ID>", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
+      printf(" : watch an ongoing game", SpacingType.REGULAR, null);
+      
+    }
+    
+    printf("clear", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
+    printf(" : clears the current terminal", SpacingType.REGULAR, null);
+    printf("quit", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
+    printf(" or ", SpacingType.NONE, null);
+    printf("exit", SpacingType.NONE, SET_TEXT_COLOR_YELLOW);
+    printf(" : exit the program", SpacingType.UNDER, null);
+  }
+
+  /**
+   * Command to log in the user.
+   *
+   * @param serverFacade serverFacade to use
+   * @param userInput user userInput to use
+   */
+  public static void loginCommand(ServerFacade serverFacade, String userInput) {
+    
+    String username = userInput.split("\\s")[1];
+    String password = userInput.split("\\s")[2];
+
+    try {
+      AuthData authData = serverFacade.loginUser(username, password);
+      authToken = authData.authToken();
+      loadValues(serverFacade);
+
+      printf(
+          "Success! You have been logged in",
+          SpacingType.SURROUND,
+          SET_TEXT_COLOR_GREEN
+      );
+      userStatus = username.toUpperCase();
+    } catch (Exception e) {
+      printf(
+          "Error: Invalid credentials",
+          SpacingType.SURROUND,
+          SET_TEXT_COLOR_RED
+      );
+    }
+  }
+
+  /**
+   * Command to log out the user.
+   *
+   * @param serverFacade serverFacade to use
+   */
+  public static void logoutCommand(ServerFacade serverFacade) {
+    try {
+      serverFacade.logoutUser(authToken);
+      authToken = "";
+      userStatus = "LOGGED_OUT";
+      printf("Logout successful!", SpacingType.SURROUND, SET_TEXT_COLOR_GREEN);
+    } catch (Exception e) {
+      printf(
+          "Error logging out: " + e.getMessage(),
+          SpacingType.SURROUND,
+          SET_TEXT_COLOR_RED
+      );
+    }
+  }
+
+  /**
+   * Command used to register a new user.
+   *
+   * @param serverFacade serverFacade to use
+   * @param userInput user userInput to use
+   */
+  public static void registerCommand(ServerFacade serverFacade, String userInput) {
+    String username = userInput.split("\\s")[1];
+    String password = userInput.split("\\s")[2];
+    String email = userInput.split("\\s")[3];
+
+    printf("\nPlease confirm Password: ", SpacingType.NONE, SET_TEXT_BOLD);
+    String confirmedPassword = SCANNER.next();
+
+    if (!password.equals(confirmedPassword)) {
+      printf(
+          "Error: Passwords do not match.",
+          SpacingType.SURROUND,
+          SET_TEXT_COLOR_RED
+      );
+      return;
+    }
+
+    try {
+      serverFacade.registerUser(username, password, email);
+
+      AuthData authData = serverFacade.loginUser(username, password);
+      authToken = authData.authToken();
+
+      printf(
+          "Success! You have been registered and logged in",
+          SpacingType.SURROUND,
+          SET_TEXT_COLOR_GREEN
+      );
+      userStatus = username.toUpperCase();
+    } catch (Exception e) {
+      if (
+          e
+              .getMessage()
+              .equals(
+                  "java.lang.RuntimeException: (403) ERROR: {\"message\":\"Error: already taken\"}"
+              )
+      ) {
+        printf(
+            "Error: Username already taken",
+            SpacingType.REGULAR,
+            SET_TEXT_COLOR_RED
+        );
+      }
+    }
+  }
+
+  /**
+   * Command used to create a new game.
+   *
+   * @param serverFacade serverFacade to use
+   * @param userInput user userInput to use
+   */
+  public static void createCommand(ServerFacade serverFacade, String userInput) {
+    String gameName = userInput.split("\\s+")[1];
+    try {
+      serverFacade.createGame(authToken, gameName);
+      loadValues(serverFacade);
+      printf(
+          "Success creating game with name '" + gameName + "'!",
+          SpacingType.SURROUND,
+          SET_TEXT_COLOR_GREEN
+      );
+    } catch (Exception e) {
+      printf(
+          "Error creating game: " + e.getMessage(),
+          SpacingType.SURROUND,
+          SET_TEXT_COLOR_RED
+      );
+    }
+  }
+
   /**
    * Command that connects the user to an active game.
    *
    * @param serverFacade serverFacade to use
-   * @param input user input to use
+   * @param userInput user userInput to use
    */
-  public static void joinCommand(ServerFacade serverFacade, String input) {
-    int listedGameId = Integer.parseInt(input.split("\\s+")[1]);
-    String playerColor = (input.split("\\s+")[2]).toUpperCase();
+  public static void joinCommand(ServerFacade serverFacade, String userInput) {
+    int listedGameId = Integer.parseInt(userInput.split("\\s+")[1]);
+    String playerColor = (userInput.split("\\s+")[2]).toUpperCase();
 
     if (listedGameId > LISTED_GAMES.size()) {
       printf(
@@ -424,6 +448,7 @@ public class ServerCommands {
   public static void observeCommand(int id) {
     if (LISTED_GAMES.containsKey(id)) {
       printf("Now observing game" + id, SpacingType.SURROUND, SET_TEXT_BOLD);
+      inGame("observer");
     } else {
       printf("Error: game does not exist", SpacingType.SURROUND, SET_TEXT_COLOR_RED);
     }
@@ -480,14 +505,5 @@ public class ServerCommands {
     printf("", SpacingType.REGULAR, null);
   }
 
-  private static void loadValues(ServerFacade serverFacade) {
-    ArrayList<GameData> games = serverFacade.listGames(authToken).games();
-    LISTED_GAMES.clear();
-    int gameNumber = 1;
 
-    for (GameData game : games) {
-      LISTED_GAMES.put(gameNumber, game.gameID());
-      gameNumber += 1;
-    }
-  }
 }
