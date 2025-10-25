9ca44b1bf1307fbd0effae2b1d84f52cc8c9ad3b
Braxten Chenay <braxten509@hotmail.com>
2025-10-24 19:00:51 -0600

    Fixed Server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,47 +3,28 @@ package server;
 import io.javalin.Javalin;
 
 public class Server {
+  private final Javalin javalin;
 
-  public Server() {}
-
-  public void run(int desiredPort) {
-    Javalin.create(javalinConfig -> javalinConfig.staticFiles.add("web"))
+  public Server() {
+    javalin = Javalin.create(javalinConfig -> javalinConfig.staticFiles.add("web"))
             .post("/user", ServerHandler::registerUser)
             .post("/session", ServerHandler::loginUser)
             .post("/game", ServerHandler::createGame)
             .put("/game", ServerHandler::joinGame)
             .get("/game", ServerHandler::listGames)
             .delete("/session", ServerHandler::logoutUser)
-            .delete("/db", ServerHandler::clearDatabase)
-            .start(desiredPort);
+            .delete("/db", ServerHandler::clearDatabase);
   }
 
-//  public int run(int desiredPort) {
-//    Spark.port(desiredPort);
-//
-//    Spark.staticFiles.location("web");
-//
-//    // Register your endpoints and handle exceptions here.
-//    Spark.post("/user", ServerHandler::registerUser);
-//    Spark.post("/session", ServerHandler::loginUser);
-//    Spark.post("/game", ServerHandler::createGame);
-//    Spark.put("/game", ServerHandler::joinGame);
-//    Spark.get("/game", ServerHandler::listGames);
-//    Spark.delete("/session", ServerHandler::logoutUser);
-//    Spark.delete("/db", ServerHandler::clearDatabase);
-//
-//    //This line initializes the server and can be removed once you have a functioning endpoint
-//    Spark.init();
-//
-//    Spark.awaitInitialization();
-//
-//    System.out.println("Success!");
-//
-//    return Spark.port();
-//  }
+  public int run(int port) {
+    javalin.start(port);
 
-//  public void stop() {
-//    Spark.stop();
-//    Spark.awaitStop();
-//  }
+    System.out.println("Server started on port: " + javalin.port());
+
+    return javalin.port();
+  }
+
+  public void stop() {
+    javalin.stop();
+  }
 }
